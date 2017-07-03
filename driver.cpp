fd9bcba7a7d64fea4893a86fae39b95e22569fab
HyperTT <HyperTT@users.noreply.github.com>
2017-07-02 23:07:11 -0700

    Create driver.cpp

---
+++ driver.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "star.h"
+using namespace std;
+
+int main()
+{
+    output_stars(1);
+    output_stars(2);
+    output_stars(5);
+    output_stars(3);
+    output_stars(4);
+    output_stars(7);
+
+    return 0;
+}
